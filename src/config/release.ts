f2abc9a40ba32f3750d81ea49de6f357c1fa17f8
Mohamed Amine AKIK <223266361+AmineAKIK@users.noreply.github.com>
2026-08-31 10:03:34 +0200

    Remove duplicate public app ID alias

--- src/config/release.ts
+++ src/config/release.ts
@@ -3,6 +3,5 @@ export const publicIndexingEnabled = false;
 export const publicBasePath = "/orianutrition/";
 export const publicOrigin = "https://amineakik.github.io";
 export const publicSiteUrl = `${publicOrigin}${publicBasePath}`;
-export const publicAppId = publicSiteUrl;
 export const publicManifestUrl = `${publicSiteUrl}manifest.webmanifest`;
 export const publicOgImageUrl = `${publicSiteUrl}pwa-512.png`;
