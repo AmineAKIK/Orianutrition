328bd51aa5beb77e3f9e1e2e6514bdf65e986990
Mohamed Amine AKIK <223266361+AmineAKIK@users.noreply.github.com>
2026-08-31 08:35:55 +0200

    Use validated social preview asset

--- src/config/release.ts
+++ src/config/release.ts
@@ -5,4 +5,4 @@ export const publicOrigin = 'https://amineakik.github.io'
 export const publicSiteUrl = `${publicOrigin}${publicBasePath}`
 export const publicAppId = publicSiteUrl
 export const publicManifestUrl = `${publicSiteUrl}manifest.webmanifest`
-export const publicOgImageUrl = `${publicSiteUrl}og-image.png`
+export const publicOgImageUrl = `${publicSiteUrl}pwa-512.png`
